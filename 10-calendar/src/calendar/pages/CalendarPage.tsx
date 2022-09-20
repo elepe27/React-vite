05615a2d61e163e0bc7d8d937eb7de74a0132dab
Esteban Eduardo Lepe Castro <esteban.lepe1c@gmail.com>
2022-09-20 13:01:32 -0400

    calendar proyect

--- 10-calendar/src/calendar/pages/CalendarPage.tsx
+++ 10-calendar/src/calendar/pages/CalendarPage.tsx
@@ -1,10 +1,55 @@
-import { Navbar } from "../components/Navbar"
+import { Calendar, dateFnsLocalizer } from 'react-big-calendar';
+import 'react-big-calendar/lib/css/react-big-calendar.css'
 
+import { addHours } from 'date-fns/esm';
+
+import { Navbar } from "../components/Navbar"
+import { getMessagesEs, localizer } from '../../helpers';
+
+const events = [{
+  title: 'Cumpleaños del jefe',
+  notes: 'Hay que comprar el pastel',
+  start: new Date(),
+  end: addHours(new Date(), 2),
+  bgColor: '#fafafa',
+  user: {
+    _id: '123',
+    name: 'Esteban'
+  }
+}]
 
 export const CalendarPage = () => {
+
+  const eventStyleGetter:any = (event: any, start: any, end: any, isSelected: any) => {
+
+    const style = {
+      backgroundColor:'#347CF7', 
+      borderRadius: '0px',
+      opacity: 0.8,
+      color: 'white'
+    }
+
+    return {
+      style
+    }
+  }
   return (
     <>
       <Navbar />
+
+      <Calendar
+        culture='es'
+        localizer={localizer}
+        events={events}
+        startAccessor="start"
+        endAccessor="end"
+        style={{ height: 'calc( 100vh - 80px )' }}
+        messages={getMessagesEs()}
+        eventPropGetter={ eventStyleGetter }
+      />
+
+
+
     </>
   )
 }
--- 10-calendar/src/main.tsx
+++ 10-calendar/src/main.tsx
@@ -1,11 +1,10 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
 
 import { CalendarApp } from './CalendarApp'
 import './styles.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
+  // <React.StrictMode>
     <CalendarApp />
-  </React.StrictMode>
+  // </React.StrictMode>
 )
